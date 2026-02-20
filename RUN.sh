ff7fa5ba5dcde947e502657db06b4fee41978c23
Anna Benýšková <anna.benyskova@gmail.com>
2026-02-20 10:14:32 +0100

    change RUN

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,9 @@
-export NXF_DISABLE_CHECK_LATEST=true
-. activate nextflow
-nextflow run /mnt/c/Users/User/kamila/WORKFLOWS/AMP/src/project/xsvato01/amp-nextflow_nf -c nextflow.config -resume 
+#!/bin/bash
+# . activate AMP-ann
+nextflow -C /mnt/hdd2/anna/AMP/src/nextflow.config run /mnt/hdd2/anna/AMP/src/project/annbeny/amp-nextflow_nf -profile high_mem -with-report -resume 
+
+# . activate nextflow 
+# nextflow run /mnt/hdd2/kristina/AMP-nextflow/src/project/xsvato01/amp-nextflow_nf -c nextflow.config -resume 
+
+# ./nextflow-24.04.4-all  kuberun kamilareblova/amplikony -r main -head-image 'cerit.io/nextflow/nextflow:24.04.4' \
+# -resume -with-report -c ./nextflow.config  --baseDir /cmbg/sequencing_results/primary_data/
